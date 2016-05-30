3c37463037d8915572ec2046c79c4c722394253e
Sunghoon <masunghoon@gmail.com>
2016-05-31 06:59:05 +0900

    1.1.2. Remove Side-effect #1
     - return `charge` as a separated value

--- src/main/scala/c1_functionalProgramming/Cafe.scala
+++ src/main/scala/c1_functionalProgramming/Cafe.scala
@@ -4,9 +4,8 @@ package c1_functionalProgramming
   * Created by masunghoon on 5/31/16.
   */
 class Cafe {
-  def buyCoffee(cc: CreditCard, p: Payments): Coffee = {
+  def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
     val cup = new Coffee()
-    p.charge(cc, cup.price)
-    cup
+    (cup, Charge(cc, cup.price))
   }
 }
