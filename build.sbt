30e7cb2e27a296e3ef437ee6d19fe4f4c150c10f
Sunghoon <masunghoon@gmail.com>
2016-05-31 06:33:58 +0900

    1.1 Program with Side-effect #2
     - added payment object

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "fpis"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.11" % "2.2.2" % "test",
+  "org.scala-lang" % "scala-reflect" % "2.11.8",
+  "org.scala-lang.modules" %% "scala-xml" % "1.0.4"
+)
--- src/main/scala/c1_functionalProgramming/Cafe.scala
+++ src/main/scala/c1_functionalProgramming/Cafe.scala
@@ -4,9 +4,9 @@ package c1_functionalProgramming
   * Created by masunghoon on 5/31/16.
   */
 class Cafe {
-  def buyCoffee(cc: CreditCard): Coffee = {
+  def buyCoffee(cc: CreditCard, p: Payments): Coffee = {
     val cup = new Coffee()
-    cc.charge(cup.price)
+    p.charge(cc, cup.price)
     cup
   }
 }
