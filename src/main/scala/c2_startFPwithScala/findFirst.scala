46f39e4acdcc14a111d4064c58e7a57a5f643455
Sunghoon Ma <masunghoon@gmail.com>
2016-06-08 06:46:47 +0900

    2.5.1 Example of polymorphic functions #2
     - 배열이나 문자열을 찾는 다형적 함수

--- src/main/scala/c2_startFPwithScala/findFirst.scala
+++ src/main/scala/c2_startFPwithScala/findFirst.scala
@@ -1,10 +1,10 @@
 object PolyMorphicFunction {
 
-  def findFirst(ss: Array[String], key: String): Int = {
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
     @annotation.tailrec
     def loop(n: Int): Int = 
-      if (n >= ss.length) -1 
-      else if (ss(n) == key) n
+      if (n >= as.length) -1 
+      else if (p(as(n))) n
       else loop(n + 1)
 
     loop(0)
