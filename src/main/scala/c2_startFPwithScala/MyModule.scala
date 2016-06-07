7b72dc64008c9c478b0ebfe57625f392f1826105
Sunghoon Ma <masunghoon@gmail.com>
2016-06-08 06:15:06 +0900

    2.4 First higher order function #2OB
    - refactoring /w higher order function

--- src/main/scala/c2_startFPwithScala/MyModule.scala
+++ src/main/scala/c2_startFPwithScala/MyModule.scala
@@ -1,14 +1,9 @@
 package c2_startFPwithScala
 
 object MyModule {
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
-  private def formatFactorial(x: Int) = {
-    val msg = "The factorial of %d is %d"
-    msg.format(x, factorial(x))
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d" 
+    msg.format(name, n, f(n))
   }
 
   def abs(n: Int): Int = if (n < 0) -n else n
@@ -23,7 +18,7 @@ object MyModule {
   }
 
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
-    println(formatFactorial(7))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial)) 
   }
 }
