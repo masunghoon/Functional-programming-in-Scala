99ceb4433e055953d35b0c6030e64ae77122ed4e
Sunghoon <masunghoon@gmail.com>
2016-06-15 06:00:53 +0900

    exercise 2.3~5 Currying, Function Composition #1

---
+++ src/main/scala/c2_startFPwithScala/Currying.scala
@@ -0,0 +1,13 @@
+package c2_startFPwithScala
+
+/** Exercise 2.3 ~ 2.5 */
+class Currying {
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+}
